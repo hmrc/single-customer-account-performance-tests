c43a7057eba858e382208e0bad3efd276b52e0a2
mathivanan-kailasam <mathivanan.kailasam@digital.hmrc.gov.uk>
2025-05-20 10:24:15 +0100

    DDCNL-10180 latest Dependancies update

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,6 +5,6 @@ object Dependencies {
   private val gatlingVersion = "3.4.2"
 
   val test = Seq(
-    "uk.gov.hmrc"          %% "performance-test-runner"   % "6.0.0"        % Test
+    "uk.gov.hmrc" %% "performance-test-runner" % "6.1.0" % Test
   )
 }
